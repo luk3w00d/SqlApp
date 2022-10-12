872938754f715c60ac4179e3380fbcec8dde89c5
luk3w00d <110910280+luk3w00d@users.noreply.github.com>
2022-10-12 14:22:59 +1000

    cards added to table

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask  #when using this the import Flask use a UPPER CASE
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
                                             # always do in this sequence and config is done at the top of the code
@@ -7,7 +8,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:passwo
 
 db = SQLAlchemy(app)
 
-class Card(db.Model):  # This will create a table that can be viewed by the users
+class Card(db.Model):      # This will create a table that can be viewed by the users
+    __tablename__ = 'cards'   # Changes the name of the name from card to cards
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text)
@@ -15,11 +17,31 @@ class Card(db.Model):  # This will create a table that can be viewed by the user
     status = db.Column(db.String)
     priority = db.Column(db.String)
 
-# Cli is used define a new custom (terminal) command
+# CLI is used define a new custom (terminal) command
 @app.cli.command('create')  # To use this command, you need to write flask then ('What word used here')
 def create_db():
     db.create_all()
     print("Tables created")  # This will print the table that was created
+
+@app.cli.command('drop')
+def drop_db():
+    db.drop_all()
+    print('Tables dropped')
+
+@app.cli.command('seed')
+def seed_db():
+    card = Card(
+        title = 'Start the project',
+        description = 'Stage 1 - Creating the database',
+        status = 'To Do',
+        priority = 'High',
+        date = date.today()
+    )
+
+    db.session.add(card)
+    db.session.commit()
+    print('Tables seeded')
+
 
 @app.route('/') 
 def index():
